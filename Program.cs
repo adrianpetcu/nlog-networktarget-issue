8444f927aeffcb027b0193eeccf8aef75fde4a55
Adrian Petcu <adrian.petcu@ubisoft.com>
2018-02-12 11:35:42 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Logger logger = LogManager.GetLogger("InfoLogger");
+
+            //logging data from an expando object
+            LogEventInfo eventInfo = GetLogEvent();
+            logger.Info(eventInfo);
+         
+            Console.WriteLine("Logging done. Press any key to exit");
+            Console.Read();
+        }
+
+
+        private static LogEventInfo GetLogEvent()
+        {
+            //construct object
+            IDictionary<string, object> loggedObject = new ExpandoObject();
+            loggedObject.Add("dbResponseTime", 50);
+            loggedObject.Add("messagesSentTotal", 100);
+
+            LogEventInfo eventInfo = new LogEventInfo(LogLevel.Info, "InfoLogger", "Test");
+            foreach (var pair in loggedObject)
+            {
+                eventInfo.Properties.Add(pair.Key, pair.Value);
+            }
+            return eventInfo;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # nlog-networktarget-issue
+
+There seems to be an issue when logging only to the network target, with JsonLayout: the fields obtained through the  event-properties renderer are missing. To reproduce:
+
+1. Start a TCP listener. Within the project, a node.js one is provided. Start it with:
+
+node listener.js
+
+2. Run the console with both targets enabled - file and network (writeTo="fileTarget,networkTarget"). The message logged by the listener should look like:
+
+{ application: 'NLogConsole',
+  timestamp: '2018-02-12T09:12:42.7514494Z',
+  machineName: 'LAB-01',
+  environment: 'DEV',
+  dbResponseTime: 50,
+  messagesSentTotal: 100,
+  module: 'core' }
+
+3. Leave the listener open. Change Nlog.config to log only to the network target (writeTo="networkTarget") and start the console again. The message received by the listener will not contain the dbResponseTime and messagesSentTotal (layout for event-properties:item):
+
+{ application: 'NLogConsole',
+  timestamp: '2018-02-12T09:13:06.8305696Z',
+  machineName: 'LAB-01',
+  environment: 'DEV',
+  module: 'core' }
+
+
+
